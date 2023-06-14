d0463808d26a56abdc0108fa64b8cfd80f21d231
Danute Liva Steimane <danutelivasteimane@gmail.com>
2023-06-14 14:04:15 +0300

    slidebar removed and js slide functions for the map and nature section added

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,35 @@
 
+
+// on loading - map scroll in the middle
 window.addEventListener('load', scroll);
 
 function scroll() {
     let map = document.querySelector('.img');
     map.scrollLeft = (map.scrollWidth - map.clientWidth) / 2;
+}
+
+// section - .slider buttons
+const btnLeft = document.querySelector('.btnLeft');
+const btnRight = document.querySelector('.btnRight');
+const carousel = document.querySelector('.carousel');
+const article = document.querySelector('article');
+
+['click', 'mouseover'].forEach(e => btnLeft.addEventListener(e, toTheLeft));
+['click', 'mouseover'].forEach(e => btnRight.addEventListener(e, toTheRight));
+
+function toTheLeft() {
+    const carouselWidth = carousel.clientWidth;
+    const articleWidth = article.clientWidth;
+    carousel.scrollLeft -= slideWidth(carouselWidth, articleWidth);
+}
+
+
+function toTheRight() {
+    const carouselWidth = carousel.clientWidth;
+    const articleWidth = article.clientWidth;
+    carousel.scrollLeft += slideWidth(carouselWidth, articleWidth)
+}
+
+function slideWidth(carousel, article) {
+    return (article + 30) * Math.floor(carousel / article);
 }
