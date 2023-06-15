110934c1c597d7bfa91da0bba9b50d90b5128d9f
Danute Liva Steimane <danutelivasteimane@gmail.com>
2023-06-15 15:10:16 +0300

    mobile navigation added

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 
-
 // on loading - map scroll in the middle
 window.addEventListener('load', scroll);
 
@@ -7,6 +6,26 @@ function scroll() {
     let map = document.querySelector('.img');
     map.scrollLeft = (map.scrollWidth - map.clientWidth) / 2;
 }
+
+
+// mobile navigation
+const nav = document.getElementById('nav');
+const btnOpenNav = document.getElementById('navOpen');
+const links = document.querySelectorAll('.link');
+
+btnOpenNav.addEventListener('click', toggleNav);
+links.forEach(link => link.addEventListener('click', toggleNav));
+
+
+function toggleNav() {
+    nav.classList.toggle('open');
+    if(nav.classList.contains('open')) {
+        btnOpenNav.innerHTML = '&#9783;';
+        return;
+    } 
+    btnOpenNav.innerHTML = '&#9776;';
+}
+
 
 
 // section - .slider buttons
@@ -22,6 +41,7 @@ function toTheLeft() {
     const carouselWidth = carousel.clientWidth;
     const articleWidth = article.clientWidth;
     carousel.scrollLeft -= slideWidth(carouselWidth, articleWidth);
+        
 }
 
 
@@ -32,5 +52,9 @@ function toTheRight() {
 }
 
 function slideWidth(carousel, article) {
-    return (article + 30) * Math.floor(carousel / article);
+    let showedArticles = Math.floor(carousel / article)
+    if (showedArticles == 0) {
+        showedArticles = 1;
+    }
+    return (article + 30) * showedArticles;
 }
