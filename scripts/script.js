602b57a3394ad8fdff38d193c2db2eee9246df32
Danute Liva Steimane <danutelivasteimane@gmail.com>
2023-06-16 23:59:03 +0300

    added first articles of nature section and corrected the scroll button function for carousel

--- scripts/script.js
+++ scripts/script.js
@@ -30,14 +30,14 @@ function toggleNav() {
 
 
 // map
-const points = document.querySelectorAll('.cul');
+const points = document.querySelectorAll('.point');
 
 points.forEach(point => point.addEventListener('click', articlePopUp));
 
 function articlePopUp(event) {
     
     
-    if (event.target.classList[0] == 'cul') {
+    if (event.target.classList[0] == 'point') {
         const article = document.getElementsByClassName(event.target.classList[1]);
         if (!document.getElementById('article-popup')) {
             const copyArticle = article[1].cloneNode(true);
@@ -53,40 +53,71 @@ function articlePopUp(event) {
     if (event.target.classList[0] == 'btn-close-popup') {
         document.getElementById('article-popup').remove();
     }
-    
-    
-    
-
 }
 
+// map random animation
 
+
+/*
+function randomGlow() {
+    const num = randomNum();
+    points[num].classList.add('glow');
+    points[randomNum()].add('glow');
+    setTimeout(function() {
+        points[num].classList.remove('glow');
+    }, 1500);
+    setTimeout(function() {
+        randomGlow();
+    }, 5000);
+}
+
+function randomNum() {
+    return Math.floor(((Math.random() * (7 - 1) + 1) - 1));
+}
+
+randomGlow();
+
+*/
 
 // section - .slider buttons
-const btnLeft = document.querySelector('.btnLeft');
-const btnRight = document.querySelector('.btnRight');
-const carousel = document.querySelector('.carousel');
+const btnLeft = document.querySelectorAll('.btnLeft');
+const btnRight = document.querySelectorAll('.btnRight');
+const carousel = document.querySelectorAll('.carousel');
 const article = document.querySelector('article');
 
-['click', 'mouseover'].forEach(e => btnLeft.addEventListener(e, toTheLeft));
-['click', 'mouseover'].forEach(e => btnRight.addEventListener(e, toTheRight));
 
-function toTheLeft() {
-    const carouselWidth = carousel.clientWidth;
+
+['click', 'mouseover'].forEach(e => btnLeft.forEach(btn => btn.addEventListener(e, toTheLeft)));
+['click', 'mouseover'].forEach(e => btnRight.forEach(btn => btn.addEventListener(e, toTheRight)));
+
+function toTheLeft(e) {
+    const section = rightSection(e);
     const articleWidth = article.clientWidth;
-    carousel.scrollLeft -= slideWidth(carouselWidth, articleWidth);       
+    carousel[section].scrollLeft -= articleWidth + 30;       
 }
 
 
-function toTheRight() {
-    const carouselWidth = carousel.clientWidth;
+function toTheRight(e) {
+    const section = rightSection(e);
     const articleWidth = article.clientWidth;
-    carousel.scrollLeft += slideWidth(carouselWidth, articleWidth)
+    carousel[section].scrollLeft += articleWidth + 30;
 }
 
+function rightSection(e) {
+    if(e.target.classList.contains('culture')) {
+        return 0;
+    }
+    return 1;
+}
+
+/*
 function slideWidth(carousel, article) {
+    
     let showedArticles = Math.floor(carousel / article)
     if (showedArticles == 0) {
         showedArticles = 1;
     }
     return (article + 30) * showedArticles;
 }
+
+*/
