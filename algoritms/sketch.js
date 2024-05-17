000ce60f01dd3e06024377687b42bac0d68829ca
danuteli <76045635+danuteli@users.noreply.github.com>
2024-05-17 15:44:04 +0300

    Adding "algoritms" section

---
+++ algoritms/sketch.js
@@ -0,0 +1,160 @@
+let dice = [1, 2, 3, 4, 5, 6];
+let x0, y0;
+let startX, startY, endX, endY;
+let x1, y1, x2, y2;
+let diceResult;
+let downDirection, rightDirection;
+
+let numSquares = 3;
+let squareSize = 600 / numSquares;
+let lineDist = squareSize / 4;
+
+let border = 50;
+let sqareStartingCoords = {
+  0: [border, border],
+  1: [border + squareSize, border],
+  2: [border + (squareSize*2), border],
+  3: [border + (squareSize * 2), border + squareSize],
+  4: [border + squareSize, border + squareSize],
+  5: [border, border + squareSize],
+  6: [border, border + (squareSize * 2)],
+  7: [border + squareSize, border + (squareSize *2)],
+  8: [border + (squareSize * 2), border + (squareSize * 2)]
+}
+
+
+
+function setup() {
+  createCanvas(700, 700);
+  frameRate(1);
+}
+
+function draw() {
+
+  background('white');
+  strokeWeight(2);
+  stroke('rgba(206, 121, 101, 1)');
+  circle(50, 50, 25);
+  circle(650, 650, 25);
+  
+
+  for (let n = 0; n < numSquares * numSquares; n++) {
+    let i = Math.floor(n / numSquares); // Row index
+    let j = n % numSquares; // Column index
+
+    x0 = (sqareStartingCoords[n])[0];
+    y0 = (sqareStartingCoords[n])[1];
+
+    // Determine the diagonal direction based on the iteration number
+    
+    if ((i + j) % 2 == 0) {
+      startX = x0;
+      startY = y0;
+      endX = x0 + squareSize;
+      endY = y0 + squareSize;
+      if ( n == 4) {
+        startX = x0 + squareSize;
+        startY = y0 + squareSize;
+        endX = x0;
+        endY = y0;
+      }
+    } else if (n == 1 || n == 7) {
+      startX = x0;
+      startY = y0 + squareSize;
+      endX = x0 + squareSize;
+      endY = y0;
+    } else {
+      startX = x0 + squareSize;
+      startY = y0;
+      endX = x0;
+      endY = y0 + squareSize;
+    }
+
+    // Fill each of the smaller squares with random lines based on dice rolls
+    x1 = startX;
+    y1 = startY;
+
+    let attempts = 0;
+
+    while (true) {
+      diceResult = random(dice);
+      downDirection = startY < endY; 
+      rightDirection = startX < endX;
+
+      switch (diceResult) {
+        case 1:
+          x2 = x1;
+          y2 = y1 + lineDist;
+          if (!downDirection || y2 > (y0 + squareSize)) {
+            y2 = y1 - lineDist;
+            if (!downDirection && y2 < y0) y2 = y1 + lineDist;
+          };
+          break;
+        case 2:
+          x2 = x1 + lineDist;
+          y2 = y1;
+          if (!rightDirection || x2 > (x0 + squareSize)) {
+            x2 = x1 - lineDist;
+            if (!rightDirection && x2 < startX) x2 = x1 + lineDist;
+          };
+          break;
+        case 3:
+          x2 = x1 - lineDist;
+          y2 = y1 + lineDist;
+          if (!downDirection || x2 < x0 || y2 > (y0 + squareSize)) {
+            x2 = x1 + lineDist;
+            y2 = y1 - lineDist;
+            if ((!downDirection && x2 > (x0 + squareSize)) || (!downDirection && y2 < y0)) {
+              x2 = x1 - lineDist;
+              y2 = y1 + lineDist;
+            }
+          }
+          break;
+        case 4:
+          x2 = x1 + lineDist;
+          y2 = y1 + lineDist;
+          if (!downDirection || x2 > (x0 + squareSize) || y2 > (y0 + squareSize)) {
+            x2 = x1 - lineDist;
+            y2 = y1 - lineDist;
+            if ((!downDirection && x2 < x0) || (!downDirection && y2 < y0)) {
+              x2 = x1 + lineDist;
+              y2 = y1 + lineDist;
+            }
+          }
+          break;
+        case 5:
+          x2 = x1 + (2 * lineDist);
+          y2 = y1;
+          if (!rightDirection || x2 > (x0 + squareSize)) {
+            x2 = x1 - (2 * lineDist);
+            if (!rightDirection && x2 < x0) x2 = x1 + (2 * lineDist);
+          };
+          break;
+        case 6:
+          x2 = x1;
+          y2 = y1 + (2 * lineDist);
+          if (!downDirection || y2 > (y0 + squareSize)) {
+            y2 = y1 - (2 * lineDist);
+            if (!downDirection && y2 < y0) y2 = y1 + (2 * lineDist);
+          }
+          break;
+      }
+
+      if ((x2 <= (x0 + squareSize) && y2 <= (y0 + squareSize)) && (x2 >= x0 && y2 >= y0)) {
+        line(x1, y1, x2, y2);
+        x1 = x2;
+        y1 = y2;
+      }
+
+      // Check if the end point is reached to break the while loop
+      if (x2 === endX && y2 === endY) {
+        break;
+      }
+    }
+  }
+}
+
+
+
+
+
